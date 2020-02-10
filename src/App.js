3fcb142c67cc984311f55e1c545505a08018acf3
Tri Vuong <tri.vuong@apple.com>
2020-02-10 22:31:20 +0700

    display chrolopeth map of cases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React, {Fragment} from "react";
+import {scaleLinear} from "d3-scale";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Sphere,
+  Graticule
+} from "react-simple-maps";
+import {min, max} from "lodash-es";
+
 import {useRemoteData} from "./hooks";
 
+const nameMappings = {
+  "United States of America": "United States",
+};
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
 function App() {
-  const {
-    loading: countriesGeoJsonLoading,
-    data: countriesGeoJson,
-    error: countriesGeoJsonError,
-  } = useRemoteData("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json");
   const {
     loading: worldLatestDataLoading,
     data: worldLatestData,
     error: worldLatestDataError,
   } = useRemoteData("https://raw.githubusercontent.com/CryptoKass/ncov-data/master/world.latest.bno.json");
 
-  if (countriesGeoJsonLoading || worldLatestDataLoading) return "Loading...";
+  if (worldLatestDataLoading) return "Loading...";
 
-  console.log({countriesGeoJson, worldLatestData});
+  const countryIndicies = Object.entries(worldLatestData.country).reduce((acc, [index, country]) => {
+    acc[country] = index;
+    return acc;
+  }, {});
 
   return (
     <Fragment>
+      <header>
+        Corona Virus World Wide Cases Map
+      </header>
+      <ComposableMap projection="geoMercator">
+        <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+        <Geographies geography={geoUrl}>
+          {
+            ({geographies}) => {
+              const casesCount = Object.values(worldLatestData.cases);
+              const maxCases = max(casesCount);
+              const minCases = min(casesCount);
+              const colorScale = scaleLinear()
+                .domain([minCases, maxCases])
+                .range(["#ffc3a0", "#ff5d00"]);
 
+              return geographies.map((geo) => {
+                const countryName = nameMappings[geo.properties.NAME] || geo.properties.NAME;
+                const countryIndex = countryIndicies[countryName];
+                const cases = worldLatestData.cases[countryIndex];
+                const props = {
+                  key: geo.rsmKey,
+                  geography: geo,
+                  fill: cases ? colorScale(cases) : "#F5F4F6",
+                };
+
+                return <Geography {...props} />;
+              });
+            }
+          }
+        </Geographies>
+      </ComposableMap>
     </Fragment>
   );
 }
